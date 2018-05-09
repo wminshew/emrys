0590747fdff3a908c88ae249a5f6457316b5330b
wminshew <wminshew@gmail.com>
2018-05-08 17:03:23 -0700

    minor Bid field update

--- pkg/job/Bid.go
+++ pkg/job/Bid.go
@@ -2,6 +2,6 @@ package job
 
 // Bid represents a minimum price or rate a miner is willing to accept to execute a job
 type Bid struct {
-	JobID string
-	Rate  float32
+	JobID   string
+	MinRate float32
 }
