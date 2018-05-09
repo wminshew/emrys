e3e849b560276cc04f3d78732f624f95f1c98a04
wminshew <wminshew@gmail.com>
2018-05-08 17:41:52 -0700

    Added creds package to hold user and miner credentials

---
+++ pkg/creds/Miner.go
@@ -0,0 +1,7 @@
+package creds
+
+// Miner allows the server to authenticate a user
+type Miner struct {
+	Email    string `json:"email",db:"email"`
+	Password string `json:"password",db:"password"`
+}
---
+++ pkg/creds/User.go
@@ -0,0 +1,7 @@
+package creds
+
+// User allows the server to authenticate a user
+type User struct {
+	Email    string `json:"email",db:"email"`
+	Password string `json:"password",db:"password"`
+}
