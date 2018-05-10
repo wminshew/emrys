f40a0a3c0e044156c5b7204f64381ebd93bc4b32
wminshew <wminshew@gmail.com>
2018-05-09 18:21:50 -0700

    added UUIDs to Jobs & Bids

--- pkg/job/Bid.go
+++ pkg/job/Bid.go
@@ -1,7 +1,13 @@
 package job
 
+import (
+	"github.com/staori/go.uuid"
+)
+
 // Bid represents a minimum price or rate a miner is willing to accept to execute a job
 type Bid struct {
-	JobID   string
+	ID      uuid.UUID
+	JobID   uuid.UUID
+	MinerID uuid.UUID
 	MinRate float64
 }
--- pkg/job/Job.go
+++ pkg/job/Job.go
@@ -1,6 +1,11 @@
 package job
 
+import (
+	"github.com/staori/go.uuid"
+)
+
 // Job represents a discrete set of computations submitted by a user to be executed by a miner
 type Job struct {
-	ID string
+	ID     uuid.UUID
+	UserID uuid.UUID
 }
