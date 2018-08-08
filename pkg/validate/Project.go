1be120532c8185dac37c656cd1a3bd2d928a3387
wminshew <wminshew@gmail.com>
2018-08-08 15:18:01 -0700

    tweaked regexp to work with gorilla mux

--- pkg/validate/Project.go
+++ pkg/validate/Project.go
@@ -1,12 +1,20 @@
 package validate
 
-import "regexp"
-
-const (
-	projectRegexp = "^[\\w-]{5,30}$"
+import (
+	"fmt"
+	"regexp"
 )
 
-// ProjectRegexp returns the regexp to validate project strings
+const (
+	projectRegexpMux = "[\\w-]{5,30}"
+)
+
+// ProjectRegexp returns the regexp to validate project strings in the client
 func ProjectRegexp() *regexp.Regexp {
-	return regexp.MustCompile(projectRegexp)
+	return regexp.MustCompile(fmt.Sprintf("^%s$", projectRegexpMux))
+}
+
+// ProjectRegexpMux returns the regexp to validate project paths with gorilla mux
+func ProjectRegexpMux() *regexp.Regexp {
+	return regexp.MustCompile(projectRegexpMux)
 }
