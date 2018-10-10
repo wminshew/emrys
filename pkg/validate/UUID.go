9d53b15c451d92e2e6633b250032a19050ee0b84
wminshew <wminshew@gmail.com>
2018-10-10 11:26:27 -0700

    added uuid string validation (though only canonical uuid format)

--- pkg/validate/UUID.go
+++ pkg/validate/UUID.go
@@ -1,12 +1,18 @@
 package validate
 
 import (
+	"fmt"
 	"regexp"
 )
 
 const (
 	uuidV4Regexp = "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}"
 )
+
+// UUIDRegexp returns the regexp to validate uuid strings in the client
+func UUIDRegexp() *regexp.Regexp {
+	return regexp.MustCompile(fmt.Sprintf("^%s$", uuidV4Regexp))
+}
 
 // UUIDRegexpMux returns the regexp to validate uuid paths with gorilla mux
 func UUIDRegexpMux() *regexp.Regexp {
