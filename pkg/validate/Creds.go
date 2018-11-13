80ff3f65c6cf8f51c6b5dedfbe07292d656925c4
wminshew <wminshew@gmail.com>
2018-11-12 18:47:08 -0800

    added email & password validation

---
+++ pkg/validate/Creds.go
@@ -0,0 +1,20 @@
+package validate
+
+import (
+	"regexp"
+)
+
+const (
+	emailRegexp    = "@"
+	passwordRegexp = "[A-Za-z0-9]{8, 30}"
+)
+
+// EmailRegexp returns the regexp to validate email strings on the server
+func EmailRegexp() *regexp.Regexp {
+	return regexp.MustCompile(emailRegexp)
+}
+
+// PasswordRegexp returns the regexp to validate password strings on the server
+func PasswordRegexp() *regexp.Regexp {
+	return regexp.MustCompile(passwordRegexp)
+}
