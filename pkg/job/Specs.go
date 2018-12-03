e02e51b0f58adc74c7fee9b2ea2a1ecf895c1b9e
wminshew <wminshew@gmail.com>
2018-12-03 15:45:09 -0800

    updated Specs

--- pkg/job/Specs.go
+++ pkg/job/Specs.go
@@ -2,12 +2,9 @@ package job
 
 // Specs represents a discrete set of specifications for a job
 type Specs struct {
-	Rate    float64 `json:"rate,omitempty"`
-	GPU     string  `json:"gpu,omitempty"`
-	RAM     uint64  `json:"ram,omitempty"`
-	ramStr  string
-	Disk    uint64 `json:"disk,omitempty"`
-	diskStr string
-	Pcie    int `json:"pcie,omitempty"`
-	pcieStr string
+	Rate float64 `json:"rate,omitempty"`
+	GPU  string  `json:"gpu,omitempty"`
+	RAM  uint64  `json:"ram,omitempty"`
+	Disk uint64  `json:"disk,omitempty"`
+	Pcie int     `json:"pcie,omitempty"`
 }
